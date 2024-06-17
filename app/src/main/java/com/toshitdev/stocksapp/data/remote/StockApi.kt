3a21ee7e79b506e301531115720914422c02a467
toshit-dh <extratdh@gmail.com>
2024-06-17 14:11:59 +0530

    structure

---
+++ app/src/main/java/com/toshitdev/stocksapp/data/remote/StockApi.kt
@@ -0,0 +1,9 @@
+package com.toshitdev.stocksapp.data.remote
+
+interface StockApi {
+    companion object{
+        const val API_KEY = "8XNU2X58Y0RRPCRL"
+        const val BASE_URL = "https://alphavantage.co/"
+
+    }
+}
